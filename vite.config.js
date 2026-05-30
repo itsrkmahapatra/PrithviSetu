f03e18aac4827bb24cc1d357a2454ecd8ef03c97
Raj Kishor Mahapatra <itsrkmahapatra@gmail.com>
2026-05-30 23:52:27 +0530

    Final fix for base path and deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/prithvisetu/',
+  base: '/PrithviSetu/',
   build: {
     target: 'esnext',
     rollupOptions: {
